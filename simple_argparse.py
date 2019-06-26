dec3588038704d237d7c171871a2386f324d8ac3
MartinMolestina <martin.molestina@gmail.com>
2019-06-26 21:59:36 +0200

    args parse example

---
+++ simple_argparse.py
@@ -0,0 +1,30 @@
+# Import required libraries 
+import argparse
+import sys
+
+# Define main function that deals with Argument Parser class
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--x', type=float, default=1.0,
+                        help='What is the first number?')
+    parser.add_argument('--y', type=float, default=1.0,
+                        help='What is the second number?')
+    parser.add_argument('--operation', type=str, default='add',
+                        help='What operation? Can choose add, sub, mul, or div')
+    args = parser.parse_args()
+    sys.stdout.write(str(calc(args)))
+
+# Simple arithmetic function    
+def calc(args):
+    if args.operation == 'add':
+        return args.x + args.y
+    elif args.operation == 'sub':
+        return args.x - args.y
+    elif args.operation == 'mul':
+        return args.x * args.y
+    elif args.operation == 'div':
+        return args.x / args.y
+
+# Check if the script is running from main
+if __name__ == '__main__':
+    main()
