143e0db5c4f968f638ee7fe51dc155b0f0026749
MartinMolestina <martin.molestina@gmail.com>
2019-07-03 19:07:22 +0200

    linear reg hard coded done

--- MachineLearning/LinearRegHardCode.py
+++ MachineLearning/LinearRegHardCode.py
@@ -5,14 +5,6 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import random
 
-# Set graphing style for better looking figures
-style.use('ggplot')
-
-# Defining simple data for testing
-## Commented since random datasets are created
-#xs = np.array([1,2,3,4,5], dtype=np.float64)
-#ys = np.array([5,4,6,5,6], dtype=np.float64)
-
 # Calculating slope and constant of the linear regression with a function
 def best_fit_slope_and_intercept(xs,ys):
     m = (((mean(xs)*mean(ys)) - mean(xs*ys)) /
@@ -45,31 +37,41 @@ def create_dataset(length=40,variance=30,step=2,correlation=True):
     xs = [x for x in range(len(ys))]
     return np.array(xs, dtype=np.float64),np.array(ys,dtype=np.float64)
 
-# Use function to create dataset
-# USER CAN MODIFY ARGS TO GET DIFFERENT RESULTS
-xs, ys = create_dataset(100,50,2,correlation='neg')
 
-# Running the function with the data
-m, b = best_fit_slope_and_intercept(xs,ys)
+if __name__ == '__main__': 
 
-# Printing the results for the user
-print(f'slope: {round(m, 3)} constant: {round(b, 3)}')
+    # Defining simple data for testing
+    ## Commented since random datasets are created
+    #xs = np.array([1,2,3,4,5], dtype=np.float64)
+    #ys = np.array([5,4,6,5,6], dtype=np.float64)
 
-# Creating a regression line for the data
-regression_line = [(m*x)+b for x in xs]
+    # Use function to create dataset
+    # USER CAN MODIFY ARGS TO GET DIFFERENT RESULTS
+    xs, ys = create_dataset(100,50,2,correlation='neg')
 
-# Predicting y with a value of x with the regression
-predict_x = 7
-predict_y = (m*predict_x)+b
-print(f'For value of x={predict_x}, y={round(predict_y, 3)}')
+    # Running the function with the data
+    m, b = best_fit_slope_and_intercept(xs,ys)
 
-# Measuring the error with R-Squared
-r_squared = coefficient_of_determination(ys,regression_line)
-print(f'R-Squared: {round(r_squared, 4)}')
+    # Printing the results for the user
+    print(f'slope: {round(m, 3)} constant: {round(b, 3)}')
 
-# Plotting the results 
-plt.scatter(xs,ys,color='#003F72',label='data')
-plt.plot(xs, regression_line, label='regression line')
-plt.scatter(predict_x, predict_y, color='#00EE72', label='prediction')
-plt.legend(loc=4)
-plt.show()
+    # Creating a regression line for the data
+    regression_line = [(m*x)+b for x in xs]
+
+    # Predicting y with a value of x with the regression
+    predict_x = 7
+    predict_y = (m*predict_x)+b
+    print(f'For value of x={predict_x}, y={round(predict_y, 3)}')
+
+    # Measuring the error with R-Squared
+    r_squared = coefficient_of_determination(ys,regression_line)
+    print(f'R-Squared: {round(r_squared, 4)}')
+
+    # Set graphing style for better looking figures
+    style.use('ggplot')
+    # Plotting the results 
+    plt.scatter(xs,ys,color='#003F72',label='data')
+    plt.plot(xs, regression_line, label='regression line')
+    plt.scatter(predict_x, predict_y, color='#00EE72', label='prediction')
+    plt.legend(loc=4)
+    plt.show()
